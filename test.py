64f42966ab0228a8bd2196fcc2578d7c04e14d17
Range <546649174@qq.com>
2017-03-15 15:59:56 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import itchat
+import time
+
+itchat.auto_login(hotReload=True)
+
+name = itchat.search_friends(wechatAccount='TheMessenger13')
+
+n = name[0]['UserName']
+print n
+if itchat.send_msg(msg='Hi stupid boy~', toUserName=n):
+    for i in xrange(0, 50):
+        itchat.send_msg(msg='Hi stupid boy~', toUserName=n)
+time.sleep(2)
+
+itchat.logout()
